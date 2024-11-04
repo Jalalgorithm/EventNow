a27fe548b63b50049d8caaea1a72d1cfcb68d59c
Jalalgorithm <temitomzi@gmail.com>
2024-11-04 22:20:35 +0100

    Components and script referenced

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Homepage from "./Homepage";
 
 function App() {
   return (
     <>
       <Header />
+      <Homepage />
       <Footer />
     </>
   );
